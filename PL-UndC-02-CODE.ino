680d5ab1ff8454d5caa2cb13e8e0d43dc426d86d
barnageller <b.geller@arduino.cc>
2019-02-13 13:00:41 +0100

    Uploading new codes #1

---
+++ PL-UndC-02-CODE.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // digitalWrite(13, HIGH);
+  digitalWrite(12, LOW);
+  // delay(500);
+  digitalWrite(13, LOW);
+  digitalWrite(12, HIGH);
+  delay(500);
+}
