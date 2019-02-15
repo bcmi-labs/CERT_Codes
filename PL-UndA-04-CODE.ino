96c2acb5d1cb34f294b5eb834c5e4482f8ee5f78
barnageller <b.geller@arduino.cc>
2019-02-15 14:07:28 +0100

    Uploading new codes #4

---
+++ PL-UndA-04-CODE.ino
@@ -0,0 +1,9 @@
+int number = random(1,10);
+if (number < 4) then
+{
+  delay(1000);
+}
+else if (number == number) then
+{ 
+  delay(500);
+}
---
+++ PL-UndA-07-CODE.ino
@@ -0,0 +1,12 @@
+int number = random(1, 10);
+while (true)
+{
+  if (number == number)
+  {
+    delay(1000);
+  }
+  else
+  {
+    delay(500);
+  }
+}
