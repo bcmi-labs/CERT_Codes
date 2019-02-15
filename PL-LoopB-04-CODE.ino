7c64533a283ee0d53703c7c7914fb8250a648a1c
barnageller <b.geller@arduino.cc>
2019-02-15 10:18:26 +0100

    removed variable interval

--- PL-LoopB-04-CODE.ino
+++ PL-LoopB-04-CODE.ino
@@ -1,11 +1,11 @@
 void loop()
 {
   current = millis();
-  while (millis() < current + interval)
+  while (millis() < current + 2000)
   {
     digitalWrite(13, HIGH);
   }
-  while (millis() < current + 2 * interval)
+  while (millis() < current + 2 * 2000)
   {
     digitalWrite(13, LOW);
   }
