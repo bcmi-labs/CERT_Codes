77472e2fce76483cd5f9fac6af6a2a4e1f01f3df
barnageller <b.geller@arduino.cc>
2019-02-15 10:44:01 +0100

    Changing code to match difficulty

--- PL-LoopB-02-CODE.ino
+++ PL-LoopB-02-CODE.ino
@@ -1,13 +1,15 @@
-for (int i = 0; i < 5; i++)
+for (int i = 1; i < 5; i++)
 {
   digitalWrite(LED1, HIGH);
   delay(250);
   digitalWrite(LED1, LOW);
   delay(250);
-  for (int i = 0; i < 5; i++)
+  for (int i = 1; i <= 7; i++)
   {
+    digitalWrite(LED1, HIGH);
     digitalWrite(LED2, HIGH);
     delay(250);
+    digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW)
     delay(250);
   }
