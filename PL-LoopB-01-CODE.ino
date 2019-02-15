94291d8deacc367df9e1cbed56b0f1d581b23dfd
barnageller <b.geller@arduino.cc>
2019-02-15 10:40:43 +0100

    indenting

--- PL-LoopB-01-CODE.ino
+++ PL-LoopB-01-CODE.ino
@@ -1,16 +1,16 @@
-  for (int i = 0; i < 5; i++)
+for (int i = 0; i < 5; i++)
+{
+  digitalWrite(LED1, HIGH);
+  delay(250);
+  digitalWrite(LED1, LOW);
+  delay(250);
+  for (int i = 26; i > 18; i--)
   {
     digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
     delay(250);
     digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
     delay(250);
-    for (int i = 26; i > 18; i--)
-    {
-      digitalWrite(LED1, HIGH);
-      digitalWrite(LED2, HIGH);
-      delay(250);
-      digitalWrite(LED1, LOW);
-      digitalWrite(LED2, LOW);
-      delay(250);
-    }  
-  }
+  }  
+}
