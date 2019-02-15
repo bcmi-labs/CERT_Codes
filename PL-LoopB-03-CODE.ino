f48c40d83e6c4fc13fc4d2732ab1507de164bce9
barnageller <b.geller@arduino.cc>
2019-02-15 09:56:00 +0100

    simplifying code #2

--- PL-LoopB-03-CODE.ino
+++ PL-LoopB-03-CODE.ino
@@ -1,35 +1,11 @@
-void loop()
+digitalWrite(LED2, HIGH);
+delay(250);
+digitalWrite(LED2, LOW);
+delay(250);;
+for (int i = 37; i > 28; i--)
 {
-  button = digitalRead(buttonPin);
-  if (button == 1) //Button pressed
-  {
-    for (int i = 0; i < 5; i++)
-    {
-      digitalWrite(LED1, HIGH);
-      delay(250);
-      digitalWrite(LED1, LOW);
-      delay(250);
-      for (int i = 0; i < 5; i++)
-      {
-        digitalWrite(LED2, HIGH);
-        delay(250);
-        digitalWrite(LED2, LOW);
-        delay(250);
-      }
-    }
-  }
-  else
-  {
-    digitalWrite(LED2, HIGH);
-    delay(250);
-    digitalWrite(LED2, LOW);
-    delay(250);;
-    for (int i = 37; i > 28; i--)
-    {
-      digitalWrite(LED2, HIGH);
-      delay(250);
-      digitalWrite(LED2, LOW);
-      delay(250);
-    }
-  }
+  digitalWrite(LED2, HIGH);
+  delay(250);
+  digitalWrite(LED2, LOW);
+  delay(250);
 }
