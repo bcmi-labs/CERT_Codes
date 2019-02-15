7ec7e4fefb1d03368c2c6dafe6819f92116e8d16
barnageller <b.geller@arduino.cc>
2019-02-15 10:36:20 +0100

    modifiying code

--- PL-LoopB-01-CODE.ino
+++ PL-LoopB-01-CODE.ino
@@ -7,8 +7,10 @@
     for (int i = 26; i > 18; i--)
     {
       digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
       delay(250);
       digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
       delay(250);
     }  
   }
