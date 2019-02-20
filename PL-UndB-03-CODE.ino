4fceb94c38d00229182f0e6ceb7838869df14fbd
barnageller <b.geller@arduino.cc>
2019-02-20 14:35:15 +0100

    formatted empty lines to fit iframe

--- PL-UndB-03-CODE.ino
+++ PL-UndB-03-CODE.ino
@@ -1,9 +1,7 @@
 void loop() {
   int sensorVal = analogRead(sensorPin);
-
   // mapping analogRead to voltage
   float voltage = (sensorVal / 1024.0) * 5;
-
   //calculating temperature
   float temperature = (voltage - 0.5) * 100;
 
