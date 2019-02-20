0915ea181e958bcff53e7d10c3f2b1596c4ea9d9
barnageller <b.geller@arduino.cc>
2019-02-20 14:34:13 +0100

    formatted conditional phrase to fit iframe

--- PL-UndB-06-CODE.ino
+++ PL-UndB-06-CODE.ino
@@ -1,5 +1,6 @@
-if ((digitalRead(B1) == 1 || digitalRead(B2) == 0)
-    || digitalRead(B3) == 1)
+if ((digitalRead(B1) == 1 || 
+     digitalRead(B2) == 0) || 
+    digitalRead(B3) == 1)
 {
   digitalWrite(4, HIGH);
 }
