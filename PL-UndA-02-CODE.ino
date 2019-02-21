1625d03779f594590d77b35d6b8377619eb73984
barnageller <b.geller@arduino.cc>
2019-02-21 17:00:18 +0100

    lowered number of lines and changed indenting

--- PL-UndA-02-CODE.ino
+++ PL-UndA-02-CODE.ino
@@ -1,16 +1,16 @@
-void loop() 
-{
-redSensorValue = analogRead(redPin);
-delay(5);
-greenSensorValue = analogRead(greenPin);
-delay(5);
-blueSensorValue = analogRead(bluePin);
-delay(5);
+void loop() {
+  redSensorValue = analogRead(redPin);
+  delay(5);
+  greenSensorValue = analogRead(greenPin);
+  delay(5);
+  blueSensorValue = analogRead(bluePin);
+  delay(5);
 
-redValue   = redSensorValue   / 4;
-greenValue = greenSensorValue / 4;
-blueValue  = blueSensorValue  / 4;
+  redValue   = redSensorValue   / 4;
+  greenValue = greenSensorValue / 4;
+  blueValue  = blueSensorValue  / 4;
 
-analogWrite(redLEDPin, redValue);
-analogWrite(greenLEDPin, greenValue);
-analogWrite(blueLEDPin, blueValue);
+  analogWrite(redLEDPin, redValue);
+  analogWrite(greenLEDPin, greenValue);
+  analogWrite(blueLEDPin, blueValue);
+}
