2792e9c0870687ae146834b5b009ac366f329b21
barnageller <b.geller@arduino.cc>
2019-02-21 16:58:59 +0100

    lowered number of lines

--- PL-LoopB-05-CODE.ino
+++ PL-LoopB-05-CODE.ino
@@ -1,11 +1,11 @@
 void setup(){
   current = millis();
   while (millis() < current + 2000)
-  {
-    digitalWrite(13, HIGH);
+  { 
+    digitalWrite(13, HIGH); 
   }
   while (millis() < current + 2 * 2000)
-  {
-    digitalWrite(13, LOW);
+  { 
+    digitalWrite(13, LOW); 
   }
 }
