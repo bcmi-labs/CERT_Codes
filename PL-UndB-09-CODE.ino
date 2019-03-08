acf24493dba83247898db4c47aa634148f98f8ce
barnageller <b.geller@arduino.cc>
2019-03-08 11:05:37 +0100

    Update PL-UndB-09-CODE.ino

--- PL-UndB-09-CODE.ino
+++ PL-UndB-09-CODE.ino
@@ -1,6 +1,6 @@
-if ((digitalRead(B1) == 0 && 
-     digitalRead(B3) == 1) || 
-    digitalRead(B2) == 0) {
+if ((digitalRead(B1) == 1 || 
+     digitalRead(B2) == 0) && 
+    digitalRead(B3) == 1) {
   digitalWrite(13, HIGH);
 }
 else {
