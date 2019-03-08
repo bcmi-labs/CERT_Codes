d211bf0cd99813442e5c84e299c819d790e9700f
barnageller <b.geller@arduino.cc>
2019-03-08 13:52:05 +0100

    added current=millis part

--- PL-UndA-01-CODE.ino
+++ PL-UndA-01-CODE.ino
@@ -1,11 +1,12 @@
-while (millis() < 5000){
-  sensorValue = analogRead(A0);
-  if (sensorValue > sensorHigh)
-  {
-    sensorHigh = sensorValue;
-  }
-  if (sensorValue < sensorLow)
-  {
-    sensorLow = sensorValue;
+void loop(){
+  current = millis();
+  while (millis() < current + 5000){
+    sensorValue = analogRead(A0);
+    
+    if (sensorValue > sensorHigh)
+    {sensorHigh = sensorValue;}
+    
+    if (sensorValue < sensorLow)
+    {sensorLow = sensorValue;}
   }
 }
