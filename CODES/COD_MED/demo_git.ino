46baa28f122a93cbb4a9c090239e22d0f93dc0fb
Rox-es <roxana.escobedo@gmail.com>
2019-03-13 17:50:43 +0100

    Add files via upload

---
+++ CODES/COD_MED/demo_git.ino
@@ -0,0 +1,11 @@
+
+  if (digitalRead(Bot1) == 1 && digitalRead(Bot3) == 0
+      || digitalRead(Bot2) == 0 && digitalRead(Bot2) == 1)
+  {
+    digitalWrite(LED, HIGH);
+  }
+
+  else
+  {
+    digitalWrite(LED, LOW);
+  }
