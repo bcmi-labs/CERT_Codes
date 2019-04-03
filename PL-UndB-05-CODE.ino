c5f5e5a9e02ca95f45e706228b4da01f2e96a288
barnageller <b.geller@arduino.cc>
2019-04-03 15:18:01 +0200

    added newlines before {

--- PL-UndB-05-CODE.ino
+++ PL-UndB-05-CODE.ino
@@ -1,8 +1,10 @@
 if ((digitalRead(B1) == 1 && 
      digitalRead(B3) == 1) || 
-    digitalRead(B2) == 1) {
+    digitalRead(B2) == 1) 
+{
   digitalWrite(13, HIGH);
 }
-else {
+else 
+{
   digitalWrite(13, LOW);
 }
