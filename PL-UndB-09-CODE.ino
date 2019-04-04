27fcf4c7ade40915c711422da2dd49608520b2ee
barnageller <b.geller@arduino.cc>
2019-04-04 14:00:53 +0200

    added init, reworked accordingly

--- PL-UndB-09-CODE.ino
+++ PL-UndB-09-CODE.ino
@@ -1,6 +1,9 @@
-if ((digitalRead(B1) == 1 || 
-     digitalRead(B2) == 0) && 
-    digitalRead(B3) == 1) 
+int pinB1 = digitalRead(11);
+int pinB2 = digitalRead(6);
+int pinB3 = digitalRead(3);
+
+if ((pinB1 == 1 || pinB2 == 0) && 
+    pinB3 == 1) 
 {
   digitalWrite(13, HIGH);
 }
