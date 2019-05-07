3f93e20310b2e8c6b8181adcdd149779cfaf7bdf
barnageller <b.geller@arduino.cc>
2019-05-07 11:51:57 +0200

    copy of LoopA-02 to rework later

---
+++ PL-LoopA-09-CODE.ino
@@ -0,0 +1,20 @@
+int count = 0;
+
+void setup(){
+  while (count < 15)
+  {
+    digitalWrite(13, HIGH);
+    delay(1250);
+    digitalWrite(13, LOW);
+    delay(1250);
+    while (count < 10)
+    {
+      digitalWrite(13, HIGH);
+      delay(1250);
+      digitalWrite(13, LOW);
+      delay(1250);
+      count++;
+    }
+  }
+}
+void loop() {}
